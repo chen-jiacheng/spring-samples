13fcdf334477bfbdddcc12cd80e63b07193299c2
chenjiacheng <chen-jiacheng@foxmail.com>
2024-07-05 00:38:24 +0800

    first commit

---
+++ spring-samples-data/src/main/java/com/chenjiacheng/spring/Main.java
@@ -0,0 +1,13 @@
+package com.chenjiacheng.spring;
+
+/**
+ * Created by chenjiacheng on 2024/7/5 00:37
+ *
+ * @author chenjiacheng
+ * @since 1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
